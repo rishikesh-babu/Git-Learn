20452e84eb8f7dda5beabe37ca9f5ad24fa7b2e1
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-07-03 13:21:49 +0530

    Updated main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Footer from './Component/Footer'
+import Layout from './Component/Layout'
 import Navbar from './Component/Navbar'
 
 function App() {
-
   return (
     <>
     <Navbar />
+    <Layout />
+    <Footer />
     </>
   )
 }
