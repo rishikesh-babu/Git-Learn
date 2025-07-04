eaca1babd0e8c7ae1c4be833b1787cc9b0844990
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-07-04 07:41:26 +0530

    updated

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-      this is nav bar, commit, commit, commit, first
+      this is nav bar, commit, commit, commit, first, second
     </div>
   )
 }
